67526c5f82fda0f6399632a36c02b3d1fee3a8ad
huhx <gohuhx@163.com>
2020-01-22 16:56:01 +0800

    [hongxiang] #002: use our customized View instead of xml file

--- app/src/main/java/com/huhx/wusq/activity/MainActivity.kt
+++ app/src/main/java/com/huhx/wusq/activity/MainActivity.kt
@@ -1,33 +1,42 @@
 package com.huhx.wusq.activity
 
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.EmbossMaskFilter
+import android.graphics.Paint
 import android.os.Bundle
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import java.util.*
+import android.util.DisplayMetrics
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    val linearLayout = LinearLayout(this)
-    super.setContentView(linearLayout)
-
-    linearLayout.orientation = LinearLayout.VERTICAL
-    val textView = TextView(this)
-    textView.text = "Hello World!"
-    textView.textSize = 20f
-
-    val button = Button(this)
-    button.text = "refresh"
-    button.textSize = 20f
-    button.setOnClickListener {
-      textView.text = "date with ${Date()}"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(MyView(this))
     }
 
-    linearLayout.addView(textView)
-    linearLayout.addView(button)
+    class MyView(context: Context?) : View(context) {
+        private val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private var embossMaskFilter: EmbossMaskFilter
+        override fun onDraw(canvas: Canvas) {
+            super.onDraw(canvas)
+            val displayMetrics = DisplayMetrics()
+            display.getMetrics(displayMetrics)
+            val width = displayMetrics.widthPixels
+            val height = displayMetrics.heightPixels
+            canvas.drawText("美", width / 10.toFloat(), height / 2 + 70.toFloat(), paint)
+        }
 
-  }
+        init {
+            paint.color = Color.RED
+            paint.style = Paint.Style.STROKE
+            paint.strokeWidth = 64f
+            paint.textSize = 1000f
+            embossMaskFilter = EmbossMaskFilter(floatArrayOf(1f, 1f, 1f), 0.05f, 5f, 13f)
+            paint.maskFilter = embossMaskFilter
+        }
+    }
 }
+
