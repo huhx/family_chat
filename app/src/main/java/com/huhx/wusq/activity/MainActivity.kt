3d8c0158495ff1ad4e2941ae3088c2228fbadfcf
huhx <gohuhx@163.com>
2020-01-22 20:37:58 +0800

    [hongxiang] #004: use TextInputLayout to manage smart hint of input

--- app/src/main/java/com/huhx/wusq/activity/MainActivity.kt
+++ app/src/main/java/com/huhx/wusq/activity/MainActivity.kt
@@ -3,12 +3,30 @@ package com.huhx.wusq.activity
 import android.app.Activity
 import android.os.Bundle
 import com.huhx.wusq.R
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        login.setOnClickListener {
+            val account = myAccount.text.toString()
+            val password = myPassword.text.toString()
+            when {
+                account.isEmpty() -> {
+                    accountWrapper.error = "username canot be blank"
+                }
+                password.isEmpty() -> {
+                    passwordWrapper.error = "password canot be blank"
+                }
+                else -> {
+                    accountWrapper.isErrorEnabled = false
+                    passwordWrapper.isErrorEnabled = false
+                }
+            }
+        }
     }
 }
 
