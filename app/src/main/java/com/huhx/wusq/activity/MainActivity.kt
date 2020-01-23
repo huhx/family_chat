a39b7af5f3c64803f59cfb7ad10677262352733d
huhx <gohuhx@163.com>
2020-01-23 08:57:52 +0800

    [hongxiang] #006: use relative to layout adjacent relationship

--- app/src/main/java/com/huhx/wusq/activity/MainActivity.kt
+++ app/src/main/java/com/huhx/wusq/activity/MainActivity.kt
@@ -2,25 +2,13 @@ package com.huhx.wusq.activity
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.GridLayout
 import com.huhx.wusq.R
-import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-
-        val titles = listOf("7", "8", "9", "+", "7", "8", "9", "+", "7", "8", "9", "+", "7", "8", "9", "+")
-        for ((index, value) in titles.withIndex()) {
-            val button = Button(this)
-            button.text = value
-            button.textSize = 36f
-            val layoutParams = GridLayout.LayoutParams(GridLayout.spec(index / 4 + 2), GridLayout.spec(index % 4))
-            myGridLayout.addView(button, layoutParams)
-        }
     }
 }
 
