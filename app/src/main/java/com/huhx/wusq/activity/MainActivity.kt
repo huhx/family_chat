e7cd9f2c619fdc8d44e93d7cc91c3e0ddca4921d
huhx <gohuhx@163.com>
2020-01-24 13:54:49 +0800

    [hongxiang] #009: use DrawerLayout

--- app/src/main/java/com/huhx/wusq/activity/MainActivity.kt
+++ app/src/main/java/com/huhx/wusq/activity/MainActivity.kt
@@ -1,33 +1,47 @@
 package com.huhx.wusq.activity
 
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import com.huhx.wusq.R
 import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        setSupportActionBar(toolbar)
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.toolbar, menu)
-        return true
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawer_layout, R.string.app_name, R.string.content)
+        drawer_layout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        nv_view.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.account -> {
+                    Toast.makeText(this, "My Account", Toast.LENGTH_SHORT).show()
+                }
+                R.id.settings -> {
+                    Toast.makeText(this, "My Setting", Toast.LENGTH_SHORT).show();
+                }
+                R.id.mycart -> {
+                    Toast.makeText(this, "My Cart", Toast.LENGTH_SHORT).show();
+                }
+            }
+            return@setNavigationItemSelectedListener true
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when (item?.itemId) {
-            R.id.backup -> Toast.makeText(this, "backup", Toast.LENGTH_SHORT).show()
-            R.id.delete -> Toast.makeText(this, "delete", Toast.LENGTH_SHORT).show()
-            R.id.settings -> Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show()
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true
         }
-        return true
+        return super.onOptionsItemSelected(item)
     }
 }
 
