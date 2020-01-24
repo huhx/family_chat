38ec51b7c05d175a725a18a5897e2b5deec8cc4f
huhx <gohuhx@163.com>
2020-01-24 09:19:15 +0800

    [hongxiang] #008: use toolbar

--- app/src/main/java/com/huhx/wusq/activity/MainActivity.kt
+++ app/src/main/java/com/huhx/wusq/activity/MainActivity.kt
@@ -1,14 +1,33 @@
 package com.huhx.wusq.activity
 
-import android.app.Activity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.huhx.wusq.R
+import kotlinx.android.synthetic.main.main_activity.*
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.backup -> Toast.makeText(this, "backup", Toast.LENGTH_SHORT).show()
+            R.id.delete -> Toast.makeText(this, "delete", Toast.LENGTH_SHORT).show()
+            R.id.settings -> Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
 
